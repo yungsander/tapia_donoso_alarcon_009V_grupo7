56373ba5bbc107965077a45cf004e3d1149146c8
andrés <dresanbeats@gmail.com>
2026-05-11 20:53:52 -0400

    base

---
+++ src/main/java/com/aduanaDonPepe/aduanaDonPepe/AduanaDonPepeApplication.java
@@ -0,0 +1,13 @@
+package com.aduanaDonPepe.aduanaDonPepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AduanaDonPepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AduanaDonPepeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aduanaDonPepe/aduanaDonPepe/AduanaDonPepeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduanaDonPepe.aduanaDonPepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AduanaDonPepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
