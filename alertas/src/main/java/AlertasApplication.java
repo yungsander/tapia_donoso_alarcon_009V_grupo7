d70549399b584fff85f70da60ff85c6eead4efe3
andrés <dresanbeats@gmail.com>
2026-06-13 23:11:18 -0400

    agg anotacion @EnableFeignClients en alertas importaciones, exportaciones e impuestos

--- alertas/src/main/java/AlertasApplication.java
+++ alertas/src/main/java/AlertasApplication.java
@@ -1,7 +1,9 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AlertasApplication {
 
 	public static void main(String[] args) {
--- exportaciones/src/main/java/exportaciones/ExportacionesApplication.java
+++ exportaciones/src/main/java/exportaciones/ExportacionesApplication.java
@@ -2,8 +2,10 @@ package exportaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ExportacionesApplication {
 
 	public static void main(String[] args) {
