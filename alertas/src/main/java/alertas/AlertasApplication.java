517b77d022d0231980d86bd25274550a23c1c702
andrés <dresanbeats@gmail.com>
2026-06-16 20:05:35 -0400

    +

--- alertas/src/main/java/alertas/AlertasApplication.java
+++ alertas/src/main/java/alertas/AlertasApplication.java
@@ -2,8 +2,15 @@ package alertas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+	"alertas", "controllers", "services", "repositories",
+	"dtos", "exceptions", "models", "config", "clients"
+})
+@EnableFeignClients(basePackages = "clients")
 public class AlertasApplication {
 
 	public static void main(String[] args) {
