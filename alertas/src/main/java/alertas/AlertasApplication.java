70039ef556cfc03f5ef471e78cad01dd399823b5
andrés <dresanbeats@gmail.com>
2026-06-16 20:21:00 -0400

    test alertas 2

--- alertas/src/main/java/alertas/AlertasApplication.java
+++ alertas/src/main/java/alertas/AlertasApplication.java
@@ -2,6 +2,7 @@ package alertas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 @EnableFeignClients(basePackages = "clients")
 @EnableJpaRepositories(basePackages = "repositories")
+@EntityScan(basePackages = "models")
 public class AlertasApplication {
 
 	public static void main(String[] args) {
