a999d1125b92ab08e5b491db040da74c3d9d61e7
andrés <dresanbeats@gmail.com>
2026-06-19 03:26:35 -0400

    test

--- alertas/src/test/java/alertas/AlertasApplicationTests.java
+++ alertas/src/test/java/alertas/AlertasApplicationTests.java
@@ -2,12 +2,18 @@ package alertas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(properties = {
     "spring.datasource.url=jdbc:mysql://localhost:3306/dummy_test_db",
     "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver",
     "spring.jpa.database-platform=org.hibernate.dialect.MySQLDialect",
     "spring.jpa.hibernate.ddl-auto=none"
+})
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "mercancia.service.url=http://localhost:8080"
 })
 class AlertasApplicationTests {
 
